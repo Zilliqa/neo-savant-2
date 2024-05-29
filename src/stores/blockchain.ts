2bceb76c384d17536801bc14a6f3c82eb5483d6a
Saeed Dadkhah <sa.dadkhah@gmail.com>
2024-05-29 16:41:57 +0330

    Fix broken menubar (#34)

--- src/stores/blockchain.ts
+++ src/stores/blockchain.ts
@@ -299,7 +299,9 @@ export const useBlockchainStore = defineStore('blockchain', {
   persist: {
     paths: ['selectedNetwork'],
     afterRestore: (context) => {
-      context.store.zilliqa = new Zilliqa(context.store.selectedNetwork.url);
+      if (context.store.selectedNetwork) {
+        context.store.zilliqa = new Zilliqa(context.store.selectedNetwork.url);
+      }
     },
   },
 });
